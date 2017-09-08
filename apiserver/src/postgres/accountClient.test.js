635bfdb046f9fdd7bb257246b12586e48b0816d0
erlangxk <simon.xikun@gmail.com>
2017-09-08 10:11:11 +0800

    add test for query user by name

--- apiserver/src/postgres/accountClient.test.js
+++ apiserver/src/postgres/accountClient.test.js
@@ -8,6 +8,7 @@ let client;
 
 const DELETE_SQL = "delete from mx_accounts";
 const QUERY_BY_ID = "select * from mx_accounts where id=$1";
+const id = "667034d0-93b6-11e7-b148-17a9dddd6b9c";
 
 function queryById(pool, id) {
     return pool.query(QUERY_BY_ID, [id]);
@@ -29,14 +30,22 @@ afterAll(() => {
 })
 
 test("insert new account into database", async() => {
-    let id = "667034d0-93b6-11e7-b148-17a9dddd6b9c";
     let result = await client.addAccount(id, "name", "passwordhash", 1);
     expect(result.rowCount).toBe(1);
     result = await queryById(pool, id);
     expect(result.rows[0]).toEqual({
-        id: '667034d0-93b6-11e7-b148-17a9dddd6b9c',
+        id,
         name: 'name',
         password_hash: 'passwordhash',
         create_time: '1'
+    });
+});
+
+test("query the user by name", async() => {
+    await client.addAccount(id, "name", "passwordhash", 1);
+    const result = await client.queryAccount("name");
+    expect(result.rows[0]).toEqual({
+        id,
+        password_hash: 'passwordhash',
     });
 });
