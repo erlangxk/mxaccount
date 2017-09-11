b895338320da7b1d88f05e3f8289856ac5df1ec5
erlangxk <simon.xikun@gmail.com>
2017-09-11 16:39:37 +0800

    change AccountService to depend on 6 functions only, and use mock functions to achieve 100% coverage

--- apiserver/src/service/AccountService.js
+++ apiserver/src/service/AccountService.js
@@ -4,29 +4,38 @@ export class AccountService {
         queryAccount,
         hashPasswd,
         verifyPasswd,
-        uuid
+        uuid,
+        currentMillis
     }) {
         this.addAccount = addAccount;
         this.queryAccount = queryAccount;
         this.hashPasswd = hashPasswd;
         this.verifyPasswd = verifyPasswd;
         this.uuid = uuid;
+        this.currentMillis = currentMillis;
     }
 
     register = async(name, password) => {
         const id = this.uuid();
         const passwordHash = await this.hashPasswd(password);
-        const millis = (new Date).getTime();
+        const millis = this.currentMillis();
         await this.addAccount(id, name, passwordHash, millis);
         return id;
     }
 
     authenticate = async(name, password) => {
         const result = await this.queryAccount(name);
-        if (result.rows.length == 1) {
-            const passwordHash = result.rows[0].password_hash;
-            return await this.verifyPasswd(password, passwordHash)
+        if (result.rows && result.rows.length == 1) {
+            const {
+                id,
+                password_hash
+            } = result.rows[0]
+            if (password_hash && id) {
+                if (await this.verifyPasswd(password, password_hash)) {
+                    return id;
+                }
+            }
         }
-        return false;
+        return null;
     }
 }
--- apiserver/src/service/AccountService.test.js
+++ apiserver/src/service/AccountService.test.js
@@ -2,45 +2,130 @@ import {
     AccountService
 } from './AccountService';
 
-import {
-    AccountClient
-} from '../postgres/Account';
-
-import {
-    Pool
-} from 'pg';
-
-import uuid from 'uuid/v1';
-
-import {
-    hashPasswd,
-    verifyPasswd
-} from '../utils/hash';
-
-let pool;
-let service;
-
 describe("test account service", () => {
-    beforeAll(() => {
-        pool = new Pool({
-            connectionString: 'postgres://postgres:111111@localhost:5432/mxaccounts',
+    test("authenticate successfully", async() => {
+        const queryAccount = (name) => {
+            return Promise.resolve({
+                rows: [{
+                    id: "123456",
+                    password_hash: "passwordhash12"
+                }]
+            });
+        }
+        const verifyPasswd = (password, hash) => {
+            return Promise.resolve(hash === 'passwordhash12' && password === 'passworddouglas');
+        }
+        const service = new AccountService({
+            queryAccount,
+            verifyPasswd
         });
-        const client=new AccountClient(pool);
-        service = new AccountService({...client,hashPasswd,verifyPasswd,uuid});
-    });
-
-    beforeEach((done) => {
-        pool.query("delete from mx_accounts").then(_ => done());
-    });
-
-    afterAll(() => {
-        pool.end();
+        const result = await service.authenticate("douglas", "passworddouglas");
+        expect(result).toBe("123456");
     })
 
-    test("register and authenticate successfully", async() => {
-        const id = await service.register("douglas", "password");
-        expect(id.length).toBe(36);
-        const result = await service.authenticate("douglas", "password");
-        expect(result).toBe(true);
+    test("authenticate failed when verifyPasswd is false", async() => {
+        const queryAccount = (name) => {
+            return Promise.resolve({
+                rows: [{
+                    id: "123456",
+                    password_hash: "passwordhash12"
+                }]
+            });
+        }
+        const verifyPasswd = (password, hash) => {
+            return Promise.resolve(false);
+        }
+        const service = new AccountService({
+            queryAccount,
+            verifyPasswd
+        });
+        const result = await service.authenticate("douglas", "passworddouglas");
+        expect(result).toBe(null);
     })
+
+    test("authenticate failed when id empty", async() => {
+        const queryAccount = (name) => {
+            return Promise.resolve({
+                rows: [{
+                    id: "",
+                    password_hash: "passwordhash12"
+                }]
+            });
+        }
+        const verifyPasswd = (password, hash) => {
+            return Promise.resolve(hash === 'passwordhash12' && password === 'passworddouglas');
+        }
+        const service = new AccountService({
+            queryAccount,
+            verifyPasswd
+        });
+        const result = await service.authenticate("douglas", "passworddouglas");
+        expect(result).toBe(null);
+    })
+
+    test("authenticate failed when password_hash empty", async() => {
+        const queryAccount = (name) => {
+            return Promise.resolve({
+                rows: [{
+                    id: "123456",
+                    password_hash: ""
+                }]
+            });
+        }
+        const verifyPasswd = (password, hash) => {
+            return Promise.resolve(hash === 'passwordhash12' && password === 'passworddouglas');
+        }
+        const service = new AccountService({
+            queryAccount,
+            verifyPasswd
+        });
+        const result = await service.authenticate("douglas", "passworddouglas");
+        expect(result).toBe(null);
+    })
+
+    test("authenticate failed when user not found", async() => {
+        const queryAccount = (name) => {
+            return Promise.resolve({
+                rows: []
+            });
+        }
+        const verifyPasswd = (password, hash) => {
+            return Promise.resolve(hash === 'passwordhash12' && password === 'passworddouglas');
+        }
+        const service = new AccountService({
+            queryAccount,
+            verifyPasswd
+        });
+        const result = await service.authenticate("douglas", "passworddouglas");
+        expect(result).toBe(null);
+    })
+
+    test("register user successfully", async() => {
+        const addAccount = (id, name, passwordHash, time) => {
+            if (name !== "douglas" || passwordHash !== "hashedpasswordxxx" || id !== "567890x" || time !== 345678) {
+                return Promise.reject(new Error(`invalid arguments:${id},${name},${passwordHash},${time}`));
+            } else {
+                return Promise.resolve(null);
+            }
+        }
+        const hashPasswd = (password) => {
+            if (password !== "passworddouglas") {
+                return Promise.reject(new Error('invalid arguments'));
+            } else {
+                return Promise.resolve("hashedpasswordxxx");
+            }
+        }
+        const uuid = () => "567890x";
+        const currentMillis = () => 345678;
+
+        const service = new AccountService({
+            addAccount,
+            hashPasswd,
+            uuid,
+            currentMillis
+        });
+
+        const result = await service.register("douglas", "passworddouglas")
+        expect(result).toBe("567890x")
+    });
 });
